c123d0e280e79708782236e2fcb43975b85fa556
unknown <sathyanarayanan0204@gmail.com>
2019-06-09 13:13:45 +0530

    master

--- src/test/java/Com/Step/Details/TestRunner.java
+++ src/test/java/Com/Step/Details/TestRunner.java
@@ -6,6 +6,6 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/Feature",dryRun = true ,glue = "Com.Step.Details" ,plugin = "html:target")
+@CucumberOptions(features = "src/test/resources/Feature",dryRun = true ,glue = "Com.Step.Details" ,plugin = "json:target/cucumber.json" )
 public class TestRunner {
 }
